dd08cb0045494007c8548d22b7b87c0e1da06992
jakubGryzio <82440709+jakubGryzio@users.noreply.github.com>
2021-04-20 00:01:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
 
     Building house = new Building();
     house.setMaterial("wood");
@@ -10,38 +12,59 @@ public class Main {
     house.setBuildIn(2000);
     house.setOwner("Jacek Sasin");
     house.setRebuild(false);
-    System.out.println(house.getMaterial() + " " + house.getHeight() + " " + house.getLength() + " " + house.getWidth() + " "
-            + house.getBuildIn() + " " + house.getOwner() + " " + house.isRebuild());
 
-    Building department = new Building();
-    department.setMaterial("concrete");
-    department.setHeight(60.0);
-    department.setLength(100.0);
-    department.setWidth(80.0);
-    department.setBuildIn(2010);
-    department.setOwner("Adam Niedzielski");
-    department.setRebuild(false);
+    Building department = new Building("concrete", 60.0, 100.0, 80.0, 2010, "Adam Niedzielski", false);
 
-    Building school = new Building();
-    school.setMaterial("concrete");
-    school.setHeight(30.0);
-    school.setLength(20.0);
-    school.setWidth(50.0);
-    school.setBuildIn(2011);
-    school.setOwner("Przemysław Czarnek");
-    school.setRebuild(true);
+    Building school = new Building("concrete", 30.0, 20.0, 50.0, 2011, "Przemysław Czarnek", true);
+
+    school.open();
+    department.close();
 
     System.out.println(house.equals(school));
-    System.out.println(department.equals(school));
     System.out.println(department.equals(department));
 
     System.out.println(house.hashCode());
-    System.out.println(school.hashCode());
-    System.out.println(department.hashCode());
 
+    System.out.println(house.getMaterial() + " " + house.getHeight() + " " + house.getLength() + " " + house.getWidth() + " "
+            + house.getBuildIn() + " " + house.getOwner() + " " + house.isRebuild());
     System.out.println(house);
-    System.out.println(department);
-    System.out.println(school);
+
+    Hospital hospital = new Hospital("concrete", 60.0, 100.0, 80.0, 2010, "Adam Niedzielski", false,
+                                    "public", 20, 150);
+    hospital.open();
+    hospital.owner();
+    System.out.println(hospital);
+        
+    System.out.println("How many are new COVID patients?");
+    int numberNewPatient = scanner.nextInt();
+    for (int i = 0; i < numberNewPatient; i++) {
+        hospital.addPatient();
+    }
+    System.out.println(hospital);
+        
+    hospital.checkEmptyBedNumber();
+    hospital.addBed(100);
+        
+    System.out.println("How many patients have recovered?");
+    int numberRecoveredPatient = scanner.nextInt();
+    for (int i = 0; i < numberRecoveredPatient; i++) {
+        hospital.dischargePatient();
+    }
+    System.out.println(hospital);
+        
+    hospital.close();
+
+        
+    Hospital hospital2 = new Hospital("concrete", 60.0, 100.0, 80.0, 2010, "Adam Niedzielski", false,
+            "private", 20, 150);
+
+    System.out.println(hospital.equals(hospital2));
+
+    if (hospital.hashCode() != hospital2.hashCode()){
+        System.out.println(true);
+    } else {
+        System.out.println("hashCode does not work!");
+    }
+
     }
 }
-
