1e71f2f1798a8c359a84aaef3b7a7a7f553545b7
TomaszStepien <82444355+tmsgbl@users.noreply.github.com>
2021-04-20 22:03:07 +0200

    Update Building.java

--- Zad1/Building.java
+++ Zad1/Building.java
@@ -112,5 +112,44 @@ public class Building {
     @Override
     public int hashCode() {
         return Objects.hash(material, height, length, width, buildIn, owner, rebuild);
+        
+        
+ 
+    public static void main(String[] args){
+    /**
+     * This is the main method which makes Building class.
+     * @param args Unused.
+     * @return Nothing.
+     *
+     */
+    Scanner scanner = new Scanner(System.in);
+
+    Building house = new Building();
+    house.setMaterial("wood");
+    house.setHeight(10.0);
+    house.setLength(20.0);
+    house.setWidth(15.0);
+    house.setBuildIn(2000);
+    house.setOwner("Jacek Sasin");
+    house.setRebuild(false);
+
+    Building department = new Building("concrete", 60.0, 100.0, 80.0, 2010, "Adam Niedzielski", false);
+
+    Building school = new Building("concrete", 30.0, 20.0, 50.0, 2011, "Przemysław Czarnek", true);
+
+    school.open();
+    department.close();
+
+    System.out.println(house.equals(school));
+    System.out.println(department.equals(department));
+
+    System.out.println(house.hashCode());
+
+    System.out.println(house.getMaterial() + " " + house.getHeight() + " " + house.getLength() + " " + house.getWidth() + " "
+            + house.getBuildIn() + " " + house.getOwner() + " " + house.isRebuild());
+    System.out.println(house);
+
+    }
+}
     }
 } 
