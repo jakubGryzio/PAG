5cd2dfa41c4589afa87c3d0742a7603a8e7c281d
tmsgbl <aloes345@gmail.com>
2021-05-23 15:24:08 +0200

    commit file

--- Zad_3/src/com/pag/zad3/Klient.java
+++ Zad_3/src/com/pag/zad3/Klient.java
@@ -3,4 +3,26 @@ package com.pag.zad3;
 public class Klient {
     private String id;
     private String name;
+
+    public Klient(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
