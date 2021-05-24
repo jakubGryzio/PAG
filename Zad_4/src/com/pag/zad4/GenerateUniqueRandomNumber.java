8f4e44e063f271a94c36f8b7ceac596a9f0b1dec
jakubGryzio <jakubgryzio@gmail.com>
2021-05-24 21:52:04 +0200

    Commit files

--- Zad_4/src/com/pag/zad4/GenerateUniqueRandomNumber.java
+++ Zad_4/src/com/pag/zad4/GenerateUniqueRandomNumber.java
@@ -1,4 +1,37 @@
 package com.pag.zad4;
+import java.util.*;
 
 public class GenerateUniqueRandomNumber {
+    private static LinkedList<Integer> randomNumber;
+
+    public static LinkedList<Integer> getRandomNumber() {
+        return randomNumber;
+    }
+
+    public static void setRandomNumber(LinkedList<Integer> randomNumber) {
+        GenerateUniqueRandomNumber.randomNumber = randomNumber;
+    }
+
+    public static void generateNumber(int MAX, int N, int S) {
+        Random random = new Random();
+        setRandomNumber(new LinkedList<>());
+        int next = -1;
+
+        while(next != S) {
+            next = random.nextInt(MAX + 1);
+            GenerateUniqueRandomNumber.randomNumber.add(next);
+            System.out.println(next);
+        }
+        if (N > GenerateUniqueRandomNumber.randomNumber.size()) {
+            N = GenerateUniqueRandomNumber.randomNumber.size();
+        }
+        System.out.println("N-number from start array");
+        for(int i = 0; i < N; i++) {
+            System.out.println(GenerateUniqueRandomNumber.randomNumber.get(i));
+        }
+        System.out.println("N-number from end array");
+        for(int i = GenerateUniqueRandomNumber.randomNumber.size() - 1; i >= GenerateUniqueRandomNumber.randomNumber.size() - N; i--) {
+            System.out.println(GenerateUniqueRandomNumber.randomNumber.get(i));
+        }
+    }
 }
---
+++ Zad_4/src/com/pag/zad4/Main.java
@@ -0,0 +1,7 @@
+package com.pag.zad4;
+
+public class Main {
+    public static void main(String[] args) {
+        GenerateUniqueRandomNumber.generateNumber(2, 1, 1);
+    }
+}
