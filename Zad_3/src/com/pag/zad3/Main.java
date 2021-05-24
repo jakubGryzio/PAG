11347ea3d0af37256ac98f291ba50eeb641b3c6c
tmsgbl <aloes345@gmail.com>
2021-05-24 19:57:27 +0200

    commit file

--- Zad_3/src/com/pag/zad3/Main.java
+++ Zad_3/src/com/pag/zad3/Main.java
@@ -28,19 +28,47 @@ public class Main {
         shop.add(chocolate);
 
         Basket basket1 = new Basket(4);
-        ProductInBasket appleIB = new ProductInBasket(apple, 5);
-        ProductInBasket chocolateIB = new ProductInBasket(chocolate, 2);
-        ProductInBasket breadIB = new ProductInBasket(bread, 2);
-        ProductInBasket blenderIB = new ProductInBasket(blender, 1);
-        basket1.push(appleIB);
-        basket1.push(chocolateIB);
-        basket1.push(breadIB);
-        basket1.push(blenderIB);
+        ProductInBasket appleIB1 = new ProductInBasket(apple, 5);
+        ProductInBasket chocolateIB1 = new ProductInBasket(chocolate, 2);
+        ProductInBasket breadIB1 = new ProductInBasket(bread, 2);
+        ProductInBasket blenderIB1 = new ProductInBasket(blender, 1);
+        basket1.push(appleIB1);
+        basket1.push(chocolateIB1);
+        basket1.push(breadIB1);
+        basket1.push(blenderIB1);
+
+        Basket basket2 = new Basket(3);
+        ProductInBasket carIB2 = new ProductInBasket(car, 1);
+        ProductInBasket vacuumIB2 = new ProductInBasket(chocolate, 1);
+        ProductInBasket breadIB2 = new ProductInBasket(bread, 3);
+        basket2.push(carIB2);
+        basket2.push(vacuumIB2);
+        basket2.push(breadIB2);
+
+        Basket basket3 = new Basket(3);
+        ProductInBasket blenderIB3 = new ProductInBasket(blender, 1);
+        ProductInBasket butterIB3 = new ProductInBasket(butter, 4);
+        ProductInBasket chocolateIB3 = new ProductInBasket(chocolate, 5);
+        basket3.push(blenderIB3);
+        basket3.push(butterIB3);
+        basket3.push(chocolateIB3);
+
+        Basket basket4 = new Basket(4);
+        ProductInBasket fridgeIB4 = new ProductInBasket(fridge, 1);
+        ProductInBasket laptopIB4 = new ProductInBasket(butter, 1);
+        ProductInBasket cookerIB4 = new ProductInBasket(cooker, 1);
+        ProductInBasket vacuumIB4 = new ProductInBasket(vacuum, 2);
+        basket4.push(fridgeIB4);
+        basket4.push(laptopIB4);
+        basket4.push(cookerIB4);
+        basket4.push(vacuumIB4);
+
+
 
         Klient klient1 = new Klient("100", "Kowalski", basket1);
-        Klient klient2 = new Klient("101", "Nowak", basket1);
-        Klient klient3 = new Klient("102", "Stępień", basket1);
-        Klient klient4 = new Klient("103", "Gryzio", basket1);
+        Klient klient2 = new Klient("101", "Nowak", basket2);
+        Klient klient3 = new Klient("102", "Stępień", basket3);
+        Klient klient4 = new Klient("103", "Gryzio", basket4);
 
         List<Klient> clients = new ArrayList<>();
         clients.add(klient1);
