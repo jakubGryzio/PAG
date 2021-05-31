73cfa0b388b72379d2115e689d99b19f1d71157f
jakubGryzio <jakubgryzio@gmail.com>
2021-05-31 21:37:41 +0200

    Commit change to ex. 5

--- zad_5/src/ConvertFromRowToColumn.java
+++ zad_5/src/ConvertFromRowToColumn.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ConvertFromRowToColumn {
     public String inputFile;
@@ -13,12 +14,46 @@ public class ConvertFromRowToColumn {
     public ConvertFromRowToColumn() {
     }
 
+    public ConvertFromRowToColumn(String in, String out) {
+        this.inputFile = in;
+        this.outputFile = out;
+    }
+
+    public String getOutputFile() {
+        return outputFile;
+    }
+
+    public void setOutputFile(String outputFile) {
+        this.outputFile = outputFile;
+    }
+
     public String getInputFile() {
         return this.inputFile;
     }
 
     public void setInputFile(String inputFile) {
         this.inputFile = inputFile;
+    }
+
+    @Override
+    public String toString() {
+        return "ConvertFromRowToColumn{" +
+                "inputFile='" + inputFile + '\'' +
+                ", outputFile='" + outputFile + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConvertFromRowToColumn that = (ConvertFromRowToColumn) o;
+        return Objects.equals(inputFile, that.inputFile) && Objects.equals(outputFile, that.outputFile);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inputFile, outputFile);
     }
 
     public String[] readFile(String arg) {
@@ -46,9 +81,13 @@ public class ConvertFromRowToColumn {
         }
 
     }
-    public static void main(String[] args) {
-        ConvertFromRowToColumn converter = new ConvertFromRowToColumn();
-        String[] numbers = converter.readFile(args[0]);
-        converter.writeFile(args[1], numbers);
+    public static void main(String[] args){
+        try {
+            ConvertFromRowToColumn converter = new ConvertFromRowToColumn();
+            String[] numbers = converter.readFile(args[0]);
+            converter.writeFile(args[1], numbers);
+        } catch (Exception ignored) {
+            System.out.println("Pass two argument! Input file path and output file path!");
+        }
     }
 }
